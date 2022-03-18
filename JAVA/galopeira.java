9b1da49a49717c6369b2149e543e126ecf5b65f7
Munyck <47032811+munyck@users.noreply.github.com>
2022-03-18 18:22:32 -0300

    Update galopeira.java

--- JAVA/galopeira.java
+++ JAVA/galopeira.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        double T;
-        String C;
-        int num = leitor.nextInt();
-        for (int i = 0; i < num; i++) {
-            C = leitor.next();
-            T = C.length();
-            System.out.printf("%.2f\n", (T / 100));
-        }
+	public static void main(String[] args)
+	{
+		Scanner leitor = new Scanner(System.in);
+		double T;
+		String C;
+		int num = leitor.nextInt();
+		for (int i = 0; i < num; i++)
+		{
+		    C = leitor.next();
+		    T = C.length();
+		    System.out.printf("%.2f\n", (T / 100));
+		}
 	}
 }
